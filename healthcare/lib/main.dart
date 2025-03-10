2e227ba6cf84dfc19062c0ddea8c28b1a4065a5f
Kameshwar051 <kamesh.a051@gmail.com>
2025-03-10 23:39:28 +0530

    added home,login and signup page

--- healthcare/lib/main.dart
+++ healthcare/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'screens/welcome_screen.dart';
+import 'screens/home_page.dart';
+import 'screens/login_page.dart';
+import 'screens/signup_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,8 +14,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      title: 'WelcomeScreen',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignUpPage(),
+      },
     );
-
   }
 }
