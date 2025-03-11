bba91a6cc2f0a3685caf50f214aa5e83c681ffd3
Kameshwar051 <kamesh.a051@gmail.com>
2025-03-11 09:17:32 +0530

    home titile changed

--- healthcare/lib/main.dart
+++ healthcare/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WelcomeScreen',
+      title: 'HomeScreen',
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/',
       routes: {
